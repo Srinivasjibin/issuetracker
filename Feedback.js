7efe6c6cf554efb86d979c9cfa9bd4a46f464be1
Srinivasjibin <88872038+Srinivasjibin@users.noreply.github.com>
2021-08-21 19:54:41 +0530

    Add files via upload

--- Feedback.js
+++ Feedback.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Button, Container, Form, Row,Nav, Card, Table,Col } from 'react-bootstrap';
 import axios from 'axios';
 import UserNavbar from './UserNavbar';
+import Feedback from "react-bootstrap/esm/Feedback";
 
 export default class ReopenIssue extends Component {
     constructor(props) {
@@ -35,33 +36,31 @@ export default class ReopenIssue extends Component {
         render(){
             <UserNavbar />
             return(
-                <div className="row" style={{backgroundImage:'url("https://cutewallpaper.org/21/solid-grey-wallpaper/light-grey-wallpapers-clicolombia.co.jpg")', backgroundSize:'cover',backgroundRepeat:'no-repeat',height:920}}>
-                <div className="col-sm-12">
-               <div align="center">
-               <div className="bg-light">
-                 <div className="col-sm-4">
-                   <label>Feedback:</label>
-                      <select name="issueFeedback" className="form-control"  value={this.state.issueFeedback}
-                        onChange={this.handleChange}>
+            <div  style={{backgroundImage:'url("https://cutewallpaper.org/21/solid-grey-wallpaper/light-grey-wallpapers-clicolombia.co.jpg")', backgroundSize:'cover',backgroundRepeat:'no-repeat',height:920}}>
+                <div align="center">
+                    <form className="bg-light" style={{border:'2px solid black',"width":700,fontFamily:'sans-serif',"padding":30}}>
+                        <div className="row" style={{"paddingLeft":220}}> 
+                            <div className="col-sm-6">
+                            <label><i class="zmdi zmdi-file-text"></i>&nbsp;Feedback:</label>
+                             <select name="issueFeedback" className="form-control"  value={this.state.issueFeedback}
+                            onChange={this.handleChange}>
                             <option value="">select</option>
                             <option value="good">good</option>
                              <option value="average">average</option>  
                              <option value="excellent">excellent</option> 
                             <option value="poor">poor</option>
-                    </select>                                      
-            </div>
-          </div>
-              <div className="">
-              <div className="col-sm-4">
-               <label>Comments :&nbsp;
-                            <i class="zmdi zmdi-edit"></i></label><br></br>
-                 <textarea rows='5' cols="50" required placeholder="" name='issueComments' value={this.state.issueComments} onChange={this.handleChange} />                       
-            </div> </div>
-            <button className="btn-primary mx-4" onClick={this.feedback}>Submit</button> 
-               
+                            </select>
+                            </div>
+                        </div>                                    
+                        <div className="col-sm-12">
+                          <label>Comments :<i class="zmdi zmdi-comment-text"></i></label><br></br>
+                                 <textarea rows='5' cols="50" required placeholder="" name='issueComments' value={this.state.issueComments} onChange={this.handleChange} />                       
+                        </div>
+                        <div className="col-sm-12">
+                                 <button className="btn-primary mx-4" onClick={this.feedback}>Submit <i class="zmdi zmdi-check"></i></button> 
+                    </div>
+            </form> </div>
                 </div>
-                </div></div>
-
             );
         }
 
