d9754cd32627277683477da80d34d563861629d1
Srinivasjibin <88872038+Srinivasjibin@users.noreply.github.com>
2021-08-24 20:44:04 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -6,13 +6,13 @@ import { BrowserRouter as Router,
 } from "react-router-dom";
 import { Button, Col, Container, ListGroup, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap';
 import LoginForm from './LoginForm';
-
+import Carousel from 'react-bootstrap/Carousel'
 class Home extends Component {
   render() {
     return (
       <><div className="row" >
       <div className="col-sm-12">
-      <div style={{backgroundImage:'url("https://wallpapercave.com/wp/wp2700080.jpg")', backgroundSize:'cover',backgroundRepeat:'no-repeat',height:870}}>
+      <div>
         <Navbar className="bg-dark" variant="dark">
           <Navbar.Brand ><h3><b>Issue Tracker</b></h3></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -34,10 +34,41 @@ class Home extends Component {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <div className="text-light text-center mt-5" >
+        <div className="text-dark text-center mt-5" >
          <h2> Welcome to Issue tracker application</h2>
         </div>
-        
+        </div>
+        <div>
+        <Carousel align="center" >
+  <Carousel.Item>
+    <img
+      className="d-block w-50"data-interval="1000"
+      src="https://www.cloudplusit.com/wp-content/uploads/2017/07/13.png"
+      alt="First slide"
+     style={{"height":700, "PaddingLeft":50,}}/>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-50 "data-interval="1000"
+      src="https://previews.123rf.com/images/jaaakworks/jaaakworks1508/jaaakworks150800020/44222401-cartoon-business-boss-giving-new-idea-to-his-employee.jpg"
+      alt="Second slide"
+      style={{"height":700, "padding":20}}/>
+  </Carousel.Item>
+  <Carousel.Item>
+  <img
+      className="d-block w-50 " data-interval="1000"
+      src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/132855224/original/0479793d74c54cd46e45e7198e2883db03d4c97f/help-you-your-computer-problems.jpeg"
+      alt="Second slide"
+      style={{"height":700, "padding":20}}/>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-50" data-interval="1000"
+      src="https://www.nicepng.com/png/detail/282-2826563_computer-clipart-thumbs-up-person-on-computer-png.png"
+      alt="Third slide"
+      style={{"height":700, "padding":20}}/>
+  </Carousel.Item>
+</Carousel>
         </div>
         <div  >
           <footer  className="bg-dark text-light" style={{"height":50}}>
